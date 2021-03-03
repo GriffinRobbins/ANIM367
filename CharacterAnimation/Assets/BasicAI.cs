2c522ee676613d32e2b22717cb27a6250321adfe
Griffin Robbins <griffin.a.robbins@gmail.com>
2021-03-03 18:15:13 -0500

    added path finding ai

---
+++ CharacterAnimation/Assets/BasicAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    public Transform player;
+
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        
+        if (points.Length == 0)
+            return;
+
+  
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+  
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void FollowPlayer()
+    {
+        agent.destination = player.transform.position;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            FollowPlayer();
+            Debug.Log("player has entered");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GotoNextPoint();
+        }
+    }
+
+    public void StopPathFinding()
+    {
+        agent.isStopped = true;
+    }
+
+    public void StartPathFinding()
+    {
+        agent.isStopped = false;
+    }
+
+
+
+}
---
+++ CharacterAnimation/Assets/StopPathFinding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopPathFinding : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponentInParent<BasicAI>().StopPathFinding();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponentInParent<BasicAI>().StartPathFinding();
+        }
+    }
+}
