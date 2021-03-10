fc1bc057239a01966bd4fa31f9157268a9026c49
Griffin Robbins <griffin.a.robbins@gmail.com>
2021-03-10 00:06:57 -0500

    worked on character controller

--- CharacterAnimation/Assets/CameraController.cs
+++ CharacterAnimation/Assets/CameraController.cs
@@ -7,22 +7,36 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float mouseSensativity;
 
     private Transform parent;
+    public Transform self;
+
+    [SerializeField]
+    private float roatation;
+    private float oldRotation;
     void Start()
     {
         parent = transform.parent;
         Cursor.lockState = CursorLockMode.Locked;
+        roatation = self.rotation.x;
     }
 
     void Update()
     {
+
         Rotate();
+        roatation = self.rotation.x;
+        if(roatation != oldRotation)
+        {
+            Debug.LogError("arrrrg");
+            oldRotation = roatation;
+        }
     }
 
     private void Rotate()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
-        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
         parent.Rotate(Vector3.up, mouseX);
-        //parent.Rotate(Vector3.left, mouseY);
+        self.Rotate(Vector3.left, mouseY);
+       
     }
 }
